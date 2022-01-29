083cf576310a03f7328d8aa61a0006aa27acefc0
ELCOT <ELCOT@Lenovo>
2022-01-28 22:01:07 -0800

    My first commit

---
+++ eve/src/eve/HelloWorld.java
@@ -0,0 +1,10 @@
+package eve;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Welcome to the programming in java");
+	}
+
+}
